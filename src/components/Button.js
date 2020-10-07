44a726d7d1862cd4e71c41defbf17f4b066db359
곽경열 <62980973+kwakky1@users.noreply.github.com>
2020-10-07 16:53:49 +0900

    test

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Button = ({Text, FontColor, OnClick}) =>  {
+    return (
+      <>
+        <button >{Text}</button>
+      </>
+    )
+
+}
+
+export default Button;
